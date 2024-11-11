7e826652713ee736d52301f62e42427e13e00096
Neel <neel.pendharkar@gmail.com>
2024-11-11 14:57:19 +0530

    Add serialization for all docs retrieved #2

--- app/server/crud/Common.py
+++ app/server/crud/Common.py
@@ -5,23 +5,31 @@ class CommonCRUD:
     def __init__(self, collection_name):
         self._collection = database.get_collection(collection_name)
 
+    def _serialize(self, doc):
+        for field in doc:
+            if field.endswith('_id'):
+                doc[field] = str(doc[field])
+            elif field.endswith('_ids'):
+                doc[field] = [str(id) for id in doc[field]]
+        return doc
+    
     async def add(self, doc: dict):
         await self._collection.insert_one(doc)
-        doc['_id'] = str(doc['_id'])
+        doc = self._serialize(doc)
 
         return doc 
 
     async def find_by_id(self, id):
-        item = await self._collection.find_one({'_id': ObjectId(id)})
-        item['_id'] = str(item['_id'])
-        return item
+        doc = await self._collection.find_one({'_id': ObjectId(id)})
+        doc = self._serialize(doc)
+        return doc
     
     async def find_all(self):
-        documents = []
-        async for document in self._collection.find({}):
-            document['_id'] = str(document['_id'])
-            documents.append(document)
-        return documents
+        docs = []
+        async for doc in self._collection.find({}):
+            doc = self._serialize(doc)
+            docs.append(doc)
+        return docs
 
     async def delete_by_id(self, id):
         try:
--- tests/test_units/test_server/test_crud/test_Common.py
+++ tests/test_units/test_server/test_crud/test_Common.py
@@ -1,6 +1,7 @@
 import pytest
 import asyncio
 from app.server.crud.Common import CommonCRUD
+from bson import ObjectId
 
 def is_subset_dict(small_dict, large_dict):
     return all(item in large_dict.items() for item in small_dict.items())
@@ -23,4 +24,26 @@ async def test_round_trip():
 
     deleted_count = await crud.delete_by_id(added_id)
     assert deleted_count == 1
+    
+@pytest.mark.asyncio()
+async def test_round_trip_with_ObjectID():
+    crud = CommonCRUD(collection_name='common')
+    oid_str = '663f0c9a521160f379f3937b'
+    doc_to_add = {'test_ids': [ObjectId(oid_str)],
+                  'test_id': ObjectId(oid_str)}
+    added_doc = await crud.add(doc_to_add)
+    
+    assert is_subset_dict(doc_to_add, added_doc)
+
+    added_id = str(added_doc['_id'])
+    added_doc = await crud.find_by_id(added_id)
+    assert added_doc['test_id'] == oid_str
+    assert added_doc['test_ids'][0] == oid_str
+    assert is_subset_dict(doc_to_add, added_doc)
+
+    docs = await crud.find_all()
+    assert added_doc in docs
+
+    # deleted_count = await crud.delete_by_id(added_id)
+    # assert deleted_count == 1
     
