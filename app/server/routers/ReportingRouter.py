32feef5cfdc12e265432de73a4f05ba78ebc94ef
Neel <neel.pendharkar@gmail.com>
2024-11-16 10:34:25 +0530

    Only get on reports

--- app/server/routers/ReportingRouter.py
+++ app/server/routers/ReportingRouter.py
@@ -9,11 +9,6 @@ from server.hash import role_required
 
 router = APIRouter()
 
-@router.post(
-    "/",
-    responses={
-        200: {"description": "Successful Response", "model": str},
-    })
 @router.get("/")
 async def get_report(current_user: dict = Depends(role_required("ADMIN"))):
     reports = await reporting_service.get_report()
